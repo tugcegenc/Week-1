38415fc974db15dcaaa99cbccac85161b7e62e3e
tugcegenc <tugceegencc@gmail.com>
2023-12-11 14:24:43 +0300

    Not ortalamasını hesaplayan program

--- NotOrtalamasiHesaplayanProgram/src/NotOrtalamaHesaplayici.java
+++ NotOrtalamasiHesaplayanProgram/src/NotOrtalamaHesaplayici.java
@@ -1,5 +1,36 @@
-public class Main {
+import java.util.Scanner;
+public class NotOrtalamaHesaplayici {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double mat, fizik, kimya, turkce, tarih, muzik; // double veri türünden altı farklı not değişkeni tanımladık.
+
+        Scanner inp = new Scanner(System.in); // Kullanıcıdan veri alabilmek için Scanner sınıfından bir nesne oluşturduk.
+
+        // Kullanıcıdan notları almak için sırasıyla isteklerde bulunduk.
+        System.out.print("Matematik Notunuz: ");
+        mat = inp.nextDouble();
+
+        System.out.print("Fizik Notunuz: ");
+        fizik = inp.nextDouble();
+
+        System.out.print("Kimya Notunuz: ");
+        kimya = inp.nextDouble();
+
+        System.out.print("Türkçe Notunuz: ");
+        turkce = inp.nextDouble();
+
+        System.out.print("Tarih Notunuz: ");
+        tarih = inp.nextDouble();
+
+        System.out.print("Müzik Notunuz: ");
+        muzik = inp.nextDouble();
+
+        double toplam = (mat + fizik + kimya + turkce + tarih + muzik); // Girilen notların toplamını hesapladık.
+        double sonuc = toplam / 6.0; // Notların ortalamasını hesapladık.
+
+        // Ortalama notun 60'tan büyük olup olmadığına bağlı olarak öğrencinin sınıfı geçip geçmediğini belirledik.
+        String str = sonuc > 60 ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(str); // Sonucu ekrana yazdırdık.
     }
 }
+
