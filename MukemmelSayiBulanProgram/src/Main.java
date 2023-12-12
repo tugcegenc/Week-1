d0e64b621db6ce86b483010852b0013db36bfa9b
tugcegenc <tugceegencc@gmail.com>
2023-12-13 00:28:34 +0300

    Mükemmel sayı bulan program

--- MukemmelSayiBulanProgram/src/Main.java
+++ MukemmelSayiBulanProgram/src/Main.java
@@ -4,23 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-
         System.out.print("Bir sayı giriniz: ");
         int number = scan.nextInt();
         int total = 0;
 
-        // Sayının bölenlerini toplar
-        for (int i = 1; i < number; i++) {
-            if (number % i == 0) {
-                total += i;
+        if (number > 0) { // Sayının bölenlerini toplar
+            for (int i = 1; i < number; i++) {
+                if (number % i == 0) {
+                    total += i;
+                }
             }
-        }
 
-
-        if (total == number) {
-            System.out.println(number + " Mükemmel sayıdır.");
+            if (total == number) {
+                System.out.println(number + " Mükemmel sayıdır.");
+            } else {
+                System.out.println(number + " Mükemmel sayı değildir.");
+            }
         } else {
-            System.out.println(number + " Mükemmel sayı değildir.");
+            System.out.println("Negatif bir sayı mükemmel sayı olamaz.");
+            return; // Programı sonlandırır
         }
     }
 }
