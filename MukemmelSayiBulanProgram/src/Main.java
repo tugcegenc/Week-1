f3f34b8ab037559047d518dd3bfe102203bb37f6
tugcegenc <tugceegencc@gmail.com>
2023-12-13 00:21:11 +0300

    Mükemmel sayı bulan program

---
+++ MukemmelSayiBulanProgram/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.print("Bir sayı giriniz: ");
+        int number = scan.nextInt();
+        int total = 0;
+
+        // Sayının bölenlerini toplar
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                total += i;
+            }
+        }
+
+
+        if (total == number) {
+            System.out.println(number + " Mükemmel sayıdır.");
+        } else {
+            System.out.println(number + " Mükemmel sayı değildir.");
+        }
+    }
+}
---
+++ MukemmelSayiBulanProgram/src/README.md
@@ -0,0 +1,9 @@
+    Mükemmel Sayı Bulan Program
+Kullanıcının girdiği bir sayının mükemmel sayı 
+olup/olmadığını bulan program
+
+    
+
+    Mükemmel Sayı Nedir ?
+Bir sayının kendisi hariç pozitif tam sayı çarpanları (kalansız bölen sayıların) toplamı kendisine eşit olan sayıya mükemmel sayı denir.
+
