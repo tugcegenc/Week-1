041e7498139bfef9d6317cc79d4b5839aa98d3cd
tugcegenc <tugceegencc@gmail.com>
2023-12-13 23:10:37 +0300

    Uçak bilet fiyatı hesaplama

--- UcakBiletiFiyatiHesaplama/src/Main.java
+++ UcakBiletiFiyatiHesaplama/src/Main.java
@@ -5,37 +5,39 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Mesafeyi (KM) girin: ");
-        int mesafe = input.nextInt();
+        int distance = input.nextInt();
 
         System.out.print("Yaşı girin: ");
-        int yas = input.nextInt();
+        int age = input.nextInt();
 
         System.out.print("Yolculuk tipini seçin (1: Tek Yön, 2: Gidiş-Dönüş): ");
-        int yolculukTipi = input.nextInt();
+        int tripType = input.nextInt();
 
-        double fiyat = mesafe * 0.10; // Mesafe başına ücret: 0.10 TL / km
+        double price = distance * 0.10; // Mesafe başına ücret: 0.10 TL / km
 
-        if (mesafe < 0 || yas < 0 || (yolculukTipi != 1 && yolculukTipi != 2)) {
+        if (distance < 0 || age < 0 || (tripType != 1 && tripType != 2)) {
             System.out.println("Hatalı Veri Girdiniz!");
         } else {
-            if (yas < 12) {
+            if (age < 12) {
                 // 12 yaşından küçükse %50 indirim
-                fiyat *= 0.5;
-            } else if (yas >= 12 && yas <= 24) {
+                price *= 0.5;
+            } else if (age >= 12 && age <= 24) {
                 // 12-24 yaş arasındaysa %10 indirim
-                fiyat *= 0.9;
-            } else if (yas >= 65) {
+                price *= 0.9;
+            } else if (age >= 65) {
                 // 65 yaşından büyükse %30 indirim
-                fiyat *= 0.7;
+                price *= 0.7;
             }
 
-            if (yolculukTipi == 2) {
+            if (tripType == 2) {
                 // Gidiş dönüş seçilmişse %20 indirim
-                fiyat *= 0.8;
+                price *= 0.8;
+                price = price * 2;    // Gidiş dönüş seçilmişse biletler 2 katı
             }
-
-            System.out.println("Toplam bilet fiyatı: " + fiyat + " TL");
+            System.out.println("Toplam bilet fiyatı: " + price + " TL");
         }
     }
+
 }
+
 
---
+++ UcakBiletiFiyatiHesaplama/src/README.md
@@ -0,0 +1,15 @@
+            Uçak Bilet Fiyatı Hesaplama
+Java ile mesafeye ve şartlara göre uçak bileti fiyatını hesaplayan program. 
+
+
+Kullanıcıdan Mesafe (KM), yaşı ve yolculuk tipi (Tek Yön, Gidiş-Dönüş) bilgilerini alınır. 
+Mesafe başına ücret 0,10 TL / km olarak hesaplanır. İlk olarak uçuşun toplam fiyatı hesaplanır ve sonrasında ki koşullara göre müşteriye aşağıdaki indirimler uygulanır.
+
+
+-Kişi 12 yaşından küçükse bilet fiyatı üzerinden %50 indirim uygulanır.
+
+-Kişi 12-24 yaşları arasında ise bilet fiyatı üzerinden %10 indirim uygulanır.
+
+-Kişi 65 yaşından büyük ise bilet fiyatı üzerinden %30 indirim uygulanır.
+
+-Kişi "Yolculuk Tipini" gidiş dönüş seçmiş ise bilet fiyatı üzerinden %20 indirim uygulanır.
